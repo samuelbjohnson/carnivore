e168afb4043c2ae30a613a1e26b9993cd6f0a0ff
Samuel B. Johnson <samuel.b.johnson@gmail.com>
2015-06-18 20:09:57 -0400

    Convert spaces to tabs

--- play-app/app/controllers/carnivore/Application.scala
+++ play-app/app/controllers/carnivore/Application.scala
@@ -2,16 +2,10 @@ package controllers.carnivore
 
 import play.api.mvc.{Action, Controller}
 
-/**
- * Created by Samuel B. Johnson on 6/18/15.
- * Unless specified elsewhere, this code is copyright 2015
- * and is licensed under the terms of the AGPL 3.0
- */
-
 class Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.carnivore.index("Carnivore"))
-  }
+	def index = Action {
+		Ok(views.html.carnivore.index("Carnivore"))
+	}
 
 }
